e2bd88a6fb1d06cefffcd8bb730eb4360f4cd60e
BennyTW <b04502051@g.ntu.edu.tw>
2019-01-01 09:59:33 +0800

    add shuffle key

--- image_encryption/key_process.py
+++ image_encryption/key_process.py
@@ -1,6 +1,6 @@
-import numpy as np
+# import numpy as np
 # import math
-from decimal import * 
+from decimal import Decimal
 import random
 
 
@@ -13,7 +13,7 @@ def getKey(file):
             data = key.split(',')
     X0 = Decimal(int(data[1])/int(data[2]))
     A = Decimal(data[0])
-    return [A,X0,int(data[3])]
+    return [A,X0,int(data[3]),float(data[4]),float(data[5])]
 
 def generateKey():
     A=round(3.5+random.getrandbits(10)/1023*0.5,6)
@@ -23,14 +23,9 @@ def generateKey():
     while Decimal(X0u/X0d)>=1 or Decimal(X0u/X0d)==0:
         X0u = random.getrandbits(1024)
         X0d = random.getrandbits(1024)
+    X0 = random.getrandbits(1024)/random.getrandbits(1024)
+    Y0 = random.getrandbits(1024)/random.getrandbits(1024)
     key_file = open("key.txt","w")
     # key_file.write("{0:.f}".format(A))
-    key_file.write("%f,%d,%d,%d"%(A,X0u,X0d,seed))
-    return [A,X0u,X0d,seed]
-    
-if __name__=="__main__":
-    # print(math.pow(2,1023)-89*math.pow(10,306)) #maximum 
-    # X0 = math.pow(2,-50) + 0.15  #is able to detect two different in 50 map    
-    # X0 = math.pow(2,-1000) + 0.15  #cannot detect two different even 500000 map
-    X0 = 0.15
-    # X0 = math.pow(2,-70) + 0.15  #cannot detect two different even 500000 map
+    key_file.write("%f,%d,%d,%d,%f,%f"%(A,X0u,X0d,seed,X0,Y0))
+    return [A, X0u, X0d, seed, X0, Y0]
